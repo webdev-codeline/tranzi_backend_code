905d1874445faf7f5ba3532388dde23ccd8c4757
webdev-codeline <rain116093@hotmail.com>
2023-10-02 19:07:35 -0700

    fix docker-compose issue and fix some migration and sql issue

--- index.js
+++ index.js
@@ -7,41 +7,78 @@ const port = 5555;
 
 const pool = new Pool({
     user: process.env.DB_USER || 'user',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'db',
     database: process.env.DB_NAME || 'citypopulations',
     password: process.env.DB_PASSWORD || 'password',
-    port: process.env.DB_PORT || 5433,
+    port: process.env.DB_PORT || 5432,
 });
-
-const redisClient = redis.createClient(process.env.REDIS_PORT || 6379, process.env.REDIS_HOST || 'localhost');
 
 app.use(express.text());
 
+
+let redisClient;
+
+(async () => {
+    redisClient = redis.createClient({
+        socket: {
+            host: 'redis',
+        },
+        database: 0
+    })
+       redisClient.on('error', err => console.log('Redis Client Error', err))
+      await  redisClient.connect();
+
+})();
+
+
 const getPopulationFromDb = async (state, city, res) => {
+    console.log('AAA');
     const cacheKey = `${state}:${city}`;
-
     // Check cache first
-    return redisClient.get(cacheKey, async (err, data) => {
-        if (data) {
-            return res.status(200).json({ population: parseInt(data) });
+    const result = await redisClient.get(cacheKey);
+    if (result) {
+        return res.status(200).json({ population: parseInt(result) });
+    }
+    try {
+        const queryResult = await pool.query('SELECT population FROM populations WHERE state = $1 AND city = $2', [state.toLowerCase(), city.toLowerCase()]);
+        console.log(queryResult);
+
+        if (queryResult.rows.length === 0) {
+            return res.status(400).send('The given state/city combination could not be found');
         }
+        const population = queryResult.rows[0].population;
+        await redisClient.set(cacheKey,population, {
+                EX: 3600,
+                NX: true,
+              
+        });  // Cache the result for 1 hour
+        return res.status(200).json({ population });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal server error');
+    }
+    // return redisClient.get(cacheKey, async (err, data) => {
+    //     console.log(data);
+    //     if (data) {
+    //         return res.status(200).json({ population: parseInt(data) });
+    //     }
 
-        // If not in cache, query the database
-        try {
-            const queryResult = await pool.query('SELECT population FROM populations WHERE state = $1 AND city = $2', [state, city]);
+    //     // If not in cache, query the database
+    //     try {
+    //         const queryResult = await pool.query('SELECT population FROM populations WHERE state = $1 AND city = $2', [state.toLowerCase(), city.toLowerCase()]);
+    //         console.log(queryResult);
 
-            if (queryResult.rows.length === 0) {
-                return res.status(400).send('The given state/city combination could not be found');
-            }
-
-            const population = queryResult.rows[0].population;
-            redisClient.setex(cacheKey, 3600, population);  // Cache the result for 1 hour
-            return res.status(200).json({ population });
-        } catch (error) {
-            console.error(error);
-            return res.status(500).send('Internal server error');
-        }
-    });
+    //         if (queryResult.rows.length === 0) {
+    //             return res.status(400).send('The given state/city combination could not be found');
+    //         }
+    //         const population = queryResult.rows[0].population;
+    //         redisClient.setex(cacheKey, 3600, population);  // Cache the result for 1 hour
+    //         return res.status(200).json({ population });
+    //     } catch (error) {
+    //         console.error(error);
+    //         return res.status(500).send('Internal server error');
+    //     }
+    // });
 };
 
 app.get('/api/population/state/:state/city/:city', async (req, res) => {
--- migrate.js
+++ migrate.js
@@ -19,11 +19,22 @@ const migrateData = async () => {
             headers: false, // Specify that there are no headers
         }))
         .on('data', async (row) => {
-            await pool.query('INSERT INTO populations (state, city, population) VALUES ($1, $2, $3)', 
-                [row[0].toLowerCase(), row[1].toLowerCase(), parseInt(row[2])]);
-      
+            results.push({ city: row[0], state: row[1], population: row[2] });
+            // console.log(row);
+
         })
         .on('end', async () => {
+            console.log(results);
+            for (const row of results) {
+
+                try {
+                    await pool.query('INSERT INTO populations (state, city, population) VALUES ($1, $2, $3)',
+                        [row.state.toLowerCase(), row.city.toLowerCase(), parseInt(row.population)]);
+                } catch (err) {
+                    console.error(err);
+                }
+
+            }
             console.log('Migration completed.');
             process.exit();
         });
